050846a1fd9d1ffa57876ffc03986dc225897029
Charles Weng <cweng8849@gmail.com>
2017-06-19 17:39:30 -0700

    adding new experimental install scripts

--- google_cloud/install_elixir144.sh
+++ google_cloud/install_elixir144.sh
@@ -6,10 +6,6 @@ sudo apt-get install tmux -y
 sudo apt-get install build-essential -y
 sudo apt-get install libncurses-dev -y
 sudo apt-get install libssl-dev -y
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-source $HOME/.bashrc
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf install elixir 1.4.4
 asdf global elixir 1.4.4
---
+++ google_cloud/install_python36.sh
@@ -0,0 +1,4 @@
+asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+asdf python install 3.6.1
+asdf global python 3.6.1
+
